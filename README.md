6a40dcb1d95775ae4d9669b421b2b43e746991e7
christianolo <cvolo@up.edu.ph>
2022-12-19 02:30:23 +0700

    setup.

---
+++ README.md
@@ -0,0 +1,9 @@
+# CMSC-165-Chromakeying
+
+# Launching the Application
+1. Download Python, opencv
+2. go to the directory where chroma.py is located.
+3. download the videos through the link text file.
+4. Extract the videos in the same folder as chroma.py.
+3. open a command prompt
+4. Run `python chroma.py`
---
+++ chroma.py
@@ -0,0 +1,47 @@
+import cv2 as cv
+import numpy as np
+ 
+video = cv.VideoCapture("extendedvid.mp4")
+ref = cv.VideoCapture("reference.mp4")
+
+frame_height = int(video.get(4))
+frame_width = int(video.get(3))
+
+out = cv.VideoWriter('output.mp4', cv.VideoWriter_fourcc(*'MJPG'), 30.0, (640,480))
+
+while True:
+ 
+    ret, frame = video.read()
+    ret2, refFrame = ref.read()
+
+    if not ret or not ret2:
+        break
+ 
+    frame = cv.resize(frame, (640, 480))
+    image = cv.resize(refFrame, (640, 480))
+    
+    frame2 = cv.cvtColor(frame.copy(), cv.COLOR_BGR2HSV)
+
+    # hsv bounds to detect the background
+    lower = np.array([30, 142, 50])
+    upper = np.array([170, 255, 255])
+    
+    mask = cv.inRange(frame2, lower, upper)
+    mask = cv.cvtColor(mask, cv.COLOR_GRAY2BGR)
+
+    # kernel = cv.getStructuringElement(cv.MORPH_ELLIPSE,((2*21-1, 2*21-1)))
+    # mask = cv.morphologyEx(mask, cv.MORPH_CLOSE, kernel)
+    # res = cv.bitwise_and(frame, frame, mask = mask)
+    
+    # f = frame - res
+    f = np.where(mask == 255, image, frame)
+ 
+    cv.imshow("mask", f)
+ 
+    if cv.waitKey(25) == 27:
+        break
+ 
+video.release()
+ref.release()
+out.release()
+cv.destroyAllWindows()
